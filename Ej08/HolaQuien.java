1290764bcc4e55eb17bce0cb38a1a2aac660a01e
Juan31452 <jcs1011@gmail.com>
2022-06-24 18:57:58 -0500

    Ej08

--- Ej08/HolaQuien.java
+++ Ej08/HolaQuien.java
@@ -8,8 +8,11 @@ public class HolaQuien {
   public static void main(String[] args) {
     var sc = new Scanner(System.in);
     System.out.println("Por favor ingrese su nombre");
-    var nombre = sc.nextLine();
-    System.out.println("Hola " + nombre + "!");
+    var nombre = sc.nextLine();//cadena-string
+    System.out.println("Por favor ingrese su edad");
+    var edad = sc.nextInt();// numerico entero
+    
+    System.out.println("Hola " + nombre + "! su edad es de : "+edad+" años");
     sc.close();
   }  
-}
+} 
