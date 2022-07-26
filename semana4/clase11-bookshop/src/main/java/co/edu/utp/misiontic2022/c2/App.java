6b0dd1bc432e1b04f206dbc2fe022b283be2ba71
Juan31452 <jcs1011@gmail.com>
2022-07-25 19:05:40 -0500

    conectar base de datos

---
+++ semana4/clase11-bookshop/src/main/java/co/edu/utp/misiontic2022/c2/App.java
@@ -0,0 +1,14 @@
+package co.edu.utp.misiontic2022.c2;
+
+import co.edu.utp.misiontic2022.c2.bookshop.BookShop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        new BookShop().start();
+    }
+
+}
