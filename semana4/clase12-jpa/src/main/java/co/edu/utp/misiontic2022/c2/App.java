a79b4a655f6f6859f66aed98d7b79a5fae255922
Juan31452 <jcs1011@gmail.com>
2022-08-02 21:51:29 -0500

    clase12-jpa

---
+++ semana4/clase12-jpa/src/main/java/co/edu/utp/misiontic2022/c2/App.java
@@ -0,0 +1,14 @@
+package co.edu.utp.misiontic2022.c2;
+
+import co.edu.utp.misiontic2022.c2.view.MainMenu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        MainMenu.start();
+    }
+
+}
