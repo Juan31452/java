db242b2c5787e7e58d4e49f722288ecf3560e347
Juan31452 <jcs1011@gmail.com>
2022-09-02 18:31:32 -0500

    se crea conexionJPA

---
+++ ConexioJPA/src/test/java/ejemplo/Conexion/ConexionApplicationTests.java
@@ -0,0 +1,13 @@
+package ejemplo.Conexion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConexionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
